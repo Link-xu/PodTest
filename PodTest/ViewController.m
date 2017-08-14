2f7b850f751cccf6213894898043dbb43d74b2a5
Link-xu <1944154449@qq.com>
2017-08-14 15:54:03 +0800

    this is PodTest
    this file for test

--- PodTest/ViewController.m
+++ PodTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "MBProgressHUD.h"
 
 @interface ViewController ()
 
@@ -16,9 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
 }
-
+- (void)setup {
+    self.view.backgroundColor = [UIColor whiteColor];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '10.0'
+target 'PodTest' do
+pod 'MBProgressHUD', '~> 1.0.0'
+end
